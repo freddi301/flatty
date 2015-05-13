9be71c0205db972cc5c7e45afc72f7a4c4492e04
Frederik Batuna <gobi301@gmail.com>
2015-05-14 00:29:45 +0200

    direction based interpolation

--- static/flatty.js
+++ static/flatty.js
@@ -10,6 +10,7 @@ function Player(name){
 			p.pullDirection();
 		} else {
 			p.others = data;
+			p.lastUpdate = new Date().valueOf();
 			p.me = p.findMe();
 		}
 	}
@@ -43,10 +44,15 @@ function Glass(id, player){
 	g.canvas.addEventListener("mousemove", function(e){
 		g.player.direction = mousePositionElementCenter(e);
 	});
-	g.drawCell = function(player){
+	g.drawCell = function(player, delta){
+		var speed = 1000/player.mass; speed = speed*(delta/300);
 		g.ctx.save();
 		g.ctx.beginPath();
-			g.ctx.arc(player.x,player.y,area2radius(player.mass),0,2*Math.PI);
+			g.ctx.arc(
+				(player.x+player.direction.x*speed),
+				(player.y+player.direction.y*speed),
+				area2radius(player.mass),
+				0,2*Math.PI);
 		g.ctx.fill();
 		g.ctx.restore();
 	}
@@ -54,16 +60,21 @@ function Glass(id, player){
 		g.ctx.save();
 		g.ctx.clearRect(0,0,g.canvas.width, g.canvas.height);
 		var halfglass = g.player.enclojure/2;
-		if (g.player.me) g.ctx.translate(-(g.player.me.x-halfglass)+(window.innerWidth/2-halfglass),
-					  -(g.player.me.y-halfglass)+(window.innerHeight/2-halfglass));
+		var delta = new Date().valueOf() - g.player.lastUpdate;
+		if (g.player.me){
+			var speed = 1000/g.player.me.mass; speed = speed*(delta/300);
+			g.ctx.translate(
+				-(g.player.me.x-halfglass+g.player.me.direction.x*speed)+(window.innerWidth/2-halfglass),
+				-(g.player.me.y-halfglass+g.player.me.direction.y*speed)+(window.innerHeight/2-halfglass));
+		}
 		for (i in players){
-			g.drawCell(players[i]);
+			g.drawCell(players[i], delta);
 		}
 		g.ctx.strokeRect(0,0,g.player.enclojure, g.player.enclojure);
 		g.ctx.restore();
 	}
 	g.animate = function(){
-		g.drawWorld(g.player.others);
+		g.drawWorld(g.player.others, g.player.lastOthers);
 		window.requestAnimationFrame(g.animate);
 	}
 	g.animate();
